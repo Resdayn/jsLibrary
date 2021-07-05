13890b21a18f930df1a720525ef765b25b38e0a6
Juan González <jgonzalez@naver.com>
2021-07-05 16:12:59 +0900

    Formatted comments and added a couple of TODOs

--- scripts/library.js
+++ scripts/library.js
@@ -1,7 +1,8 @@
+// Array containing the individual book objects
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
-  // the constructor...
+  // Constructor for the a book object
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -9,30 +10,35 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary(book) {
-  // do stuff here
+  // Adds a book to the library array
   myLibrary.push(book);
 }
 
+// Prevents the default form behavior
 const form = document.querySelector("#form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 })
 
+// Adds an event listener to the "Add a book" button in the form.
+// It collects the info from the form and add them as parameters for the book constructor.
 const addInput = document.querySelector("#add-input");
-
 addInput.addEventListener("click", () => {
-  console.log("submit button pressed!");
-  let title = document.querySelector('[name="title"]');
-  let author = document.querySelector('[name="author"]');
-  let pages = document.querySelector('[name="pages"]');
-  let isRead = document.querySelector('[name="read"]');
+    console.log("submit button pressed!");
+    let title = document.querySelector('[name="title"]');
+    let author = document.querySelector('[name="author"]');
+    let pages = document.querySelector('[name="pages"]');
+    let isRead = document.querySelector('[name="read"]');
 
-  let newBook = new Book(title.value, author.value, pages.value, isRead.checked);
-  addBookToLibrary(newBook);
+    let newBook = new Book(title.value, author.value, pages.value, isRead.checked);
+    addBookToLibrary(newBook);
 
-  title.value = "";
-  author.value = "";
-  pages.value = "";
-  isRead.checked = false;
+    // TODO: Turn this into a function to refresh the forms boxes values to empty
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    isRead.checked = false;
+
+    // TODO: Create a function to create a div per book in the array and append it to #books-display
 
 })
