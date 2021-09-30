f7c3f1b4503321e858a0b309f09790bae0f297db
Juan Gonzalez <jgonzalez@naver.com>
2021-09-30 12:17:00 +0900

    fixed bug not showing the number of pages in the card

--- scripts/library.js
+++ scripts/library.js
@@ -2,22 +2,22 @@
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
-  // Constructor for the a book object
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead
+    // Constructor for the a book object
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead
 }
 
 function addBookToLibrary(book) {
-  // Adds a book to the library array
-  myLibrary.push(book);
+    // Adds a book to the library array
+    myLibrary.push(book);
 }
 
 // Prevents the default form behavior
 const form = document.querySelector("#form");
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 })
 
 // Adds an event listener to the "Add a book" button in the form.
@@ -30,8 +30,8 @@ addInput.addEventListener("click", () => {
     let pages = document.querySelector('[name="pages"]');
     let isRead = document.querySelector('[name="read"]');
 
-    if (title.value == '' || author.value == '' || pages.value == ''){
-      return alert('You need to fill all the sections in order to add a book!');
+    if (title.value == '' || author.value == '' || pages.value == '') {
+        return alert('You need to fill all the sections in order to add a book!');
     }
 
     let newBook = new Book(title.value, author.value, pages.value, isRead.checked);
@@ -45,91 +45,91 @@ addInput.addEventListener("click", () => {
 
     // Removes all the html book cards and runs addBookToDisplay
     let bookDisplay = document.querySelector('#books-display');
-    while (bookDisplay.firstChild){
-      bookDisplay.removeChild(bookDisplay.firstChild)
+    while (bookDisplay.firstChild) {
+        bookDisplay.removeChild(bookDisplay.firstChild)
     }
     myLibrary.forEach((book, index) => addBookToDisplay(book, index));
 
 })
 
-function addBookToDisplay(book, index){
-  // Takes a book object, creates a div with its details, applies a css class and appends it to the #book-display container
-  const bookDiv = document.createElement('div');
+function addBookToDisplay(book, index) {
+    // Takes a book object, creates a div with its details, applies a css class and appends it to the #book-display container
+    const bookDiv = document.createElement('div');
 
-  // Adds the book div to the DOM and applies the CSS style
-  document.getElementById('books-display').appendChild(bookDiv);
-  bookDiv.classList.add("book");
+    // Adds the book div to the DOM and applies the CSS style
+    document.getElementById('books-display').appendChild(bookDiv);
+    bookDiv.classList.add("book");
 
-  // Adds the data-attribute with the index value in the library array
-  bookDiv.dataset.bookIndex = index;
+    // Adds the data-attribute with the index value in the library array
+    bookDiv.dataset.bookIndex = index;
 
-  // Adds the 4 divs for each of the book properties
-  let bookTitle = document.createElement('div');
-  bookTitle.id = "book-title";
-  bookTitle.innerText = book.title
-  bookTitle.style.fontSize = '3em';
-  bookDiv.append(bookTitle);
-  
-  let bookAuthor = document.createElement('div');
-  bookAuthor.id = "book-author";
-  bookAuthor.innerText = book.author;
-  bookAuthor.style.fontSize = '2em';
-  bookDiv.append(bookAuthor);
+    // Adds the 4 divs for each of the book properties
+    let bookTitle = document.createElement('div');
+    bookTitle.id = "book-title";
+    bookTitle.innerText = book.title
+    bookTitle.style.fontSize = '3em';
+    bookDiv.append(bookTitle);
 
-  let bookPages = document.createElement('div');
-  bookPages.id = "book-pages";
-  bookPages.innerText = `${book.pages} pages`;
-  bookPages.style.fontSize = '2em';
-  bookDiv.append(bookAuthor);  
+    let bookAuthor = document.createElement('div');
+    bookAuthor.id = "book-author";
+    bookAuthor.innerText = book.author;
+    bookAuthor.style.fontSize = '2em';
+    bookDiv.append(bookAuthor);
 
-  let bookIsread = document.createElement('div');
-  bookIsread.id = "book-Isread";
-  if (book.isRead == false){
-    bookIsread.innerText = 'Not Read';
-  } else {bookIsread.innerText = 'Finished';}
-  
-  bookIsread.style.fontSize = '2em';
-  bookDiv.append(bookIsread);
+    let bookPages = document.createElement('div');
+    bookPages.id = "book-pages";
+    bookPages.innerText = `${book.pages} pages`;
+    bookPages.style.fontSize = '2em';
+    bookDiv.append(bookPages);
 
-  // Adds the button to change the read value
-  let changeReadStatus = document.createElement('button');
-  changeReadStatus.id = 'change-read';
-  changeReadStatus.classList.add('read-change-button')
-  bookDiv.append(changeReadStatus)
+    let bookIsread = document.createElement('div');
+    bookIsread.id = "book-Isread";
+    if (book.isRead == false) {
+        bookIsread.innerText = 'Not Read';
+    } else { bookIsread.innerText = 'Finished'; }
 
-  if (bookIsread.innerText = 'Not Read'){
-    changeReadStatus.innerText = 'Finished';
-  } else if (bookIsread.innerText = 'Finished'){
-    changeReadStatus.innerText = 'Not Read';
-  }
-  
-  changeReadStatus.addEventListener('click', (e) => {
-    // Changes the read status in the html and changes the read value of the book object in the array
-    if (book.isRead == false){
-      e.target.parentElement.children[2].innerText = 'Finished';
-      myLibrary[index].isRead = true;
-      changeReadStatus.innerText = 'Not Read';
-    } else {
-      e.target.parentElement.children[2].innerText = 'Not Read';
-      myLibrary[index].isRead = false;
-      changeReadStatus.innerText = 'Finished';
+    bookIsread.style.fontSize = '2em';
+    bookDiv.append(bookIsread);
+
+    // Adds the button to change the read value
+    let changeReadStatus = document.createElement('button');
+    changeReadStatus.id = 'change-read';
+    changeReadStatus.classList.add('read-change-button')
+    bookDiv.append(changeReadStatus)
+
+    if (bookIsread.innerText = 'Not Read') {
+        changeReadStatus.innerText = 'Finished';
+    } else if (bookIsread.innerText = 'Finished') {
+        changeReadStatus.innerText = 'Not Read';
     }
-  });
 
-  // Adds the button to remove the book
-  let removeBookButton = document.createElement('button');
-  removeBookButton.id = 'remove-button';
-  removeBookButton.innerText = 'Remove';
-  removeBookButton.classList.add('remove-button');
-  bookDiv.append(removeBookButton);
-  removeBookButton.addEventListener('click', (e) => {
-    console.log(`The index of this book is ${e.target.parentElement.dataset.bookIndex}`);
-    delete myLibrary[e.target.parentElement.dataset.bookIndex];
-    let bookDisplay = document.querySelector('#books-display');
-    while (bookDisplay.firstChild){
-      bookDisplay.removeChild(bookDisplay.firstChild)
-    }
-    myLibrary.forEach((book, index) => addBookToDisplay(book, index));
-  });
+    changeReadStatus.addEventListener('click', (e) => {
+        // Changes the read status in the html and changes the read value of the book object in the array
+        if (book.isRead == false) {
+            e.target.parentElement.children[2].innerText = 'Finished';
+            myLibrary[index].isRead = true;
+            changeReadStatus.innerText = 'Not Read';
+        } else {
+            e.target.parentElement.children[2].innerText = 'Not Read';
+            myLibrary[index].isRead = false;
+            changeReadStatus.innerText = 'Finished';
+        }
+    });
+
+    // Adds the button to remove the book
+    let removeBookButton = document.createElement('button');
+    removeBookButton.id = 'remove-button';
+    removeBookButton.innerText = 'Remove';
+    removeBookButton.classList.add('remove-button');
+    bookDiv.append(removeBookButton);
+    removeBookButton.addEventListener('click', (e) => {
+        console.log(`The index of this book is ${e.target.parentElement.dataset.bookIndex}`);
+        delete myLibrary[e.target.parentElement.dataset.bookIndex];
+        let bookDisplay = document.querySelector('#books-display');
+        while (bookDisplay.firstChild) {
+            bookDisplay.removeChild(bookDisplay.firstChild)
+        }
+        myLibrary.forEach((book, index) => addBookToDisplay(book, index));
+    });
 
 }
