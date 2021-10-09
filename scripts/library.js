96d4e42ccb29010bd958c947a56950d78d1fe391
Juan Gonzalez <jgonzalez@naver.com>
2021-10-09 14:35:01 +0900

    fixed bug with the assignment of isRead status in the BookDiv

--- scripts/library.js
+++ scripts/library.js
@@ -30,18 +30,13 @@ addInput.addEventListener("click", () => {
     let pages = document.querySelector('[name="pages"]');
     let isRead = document.querySelector('[name="read"]');
 
+    // Simple form validation
     if (title.value == '' || author.value == '' || pages.value == '') {
         return alert('You need to fill all the sections in order to add a book!');
     }
 
     let newBook = new Book(title.value, author.value, pages.value, isRead.checked);
     addBookToLibrary(newBook);
-
-    // TODO: Turn this into a function to refresh the forms boxes values to empty
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    isRead.checked = false;
 
     // Removes all the html book cards and runs addBookToDisplay
     let bookDisplay = document.querySelector('#books-display');
@@ -50,10 +45,17 @@ addInput.addEventListener("click", () => {
     }
     myLibrary.forEach((book, index) => addBookToDisplay(book, index));
 
+    // TODO: Turn this into a function to refresh the forms boxes values to empty
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    isRead.checked = false;
+
 })
 
 function addBookToDisplay(book, index) {
-    // Takes a book object, creates a div with its details, applies a css class and appends it to the #book-display container
+    // Takes a book object from myLibrary array, 
+    // creates a div with its details, applies a css class and appends it to the #book-display container
     const bookDiv = document.createElement('div');
 
     // Adds the book div to the DOM and applies the CSS style
@@ -65,54 +67,62 @@ function addBookToDisplay(book, index) {
 
     // Adds the 4 divs for each of the book properties
     let bookTitle = document.createElement('div');
-    bookTitle.id = "book-title";
+    bookTitle.id = `book-title-${Math.floor(Math.random() * 100)}`;
     bookTitle.innerText = book.title
     bookTitle.style.fontSize = '3em';
     bookDiv.append(bookTitle);
 
     let bookAuthor = document.createElement('div');
-    bookAuthor.id = "book-author";
+    bookAuthor.id = `book-author-${Math.floor(Math.random() * 100)}`;
     bookAuthor.innerText = book.author;
     bookAuthor.style.fontSize = '2em';
     bookDiv.append(bookAuthor);
 
     let bookPages = document.createElement('div');
-    bookPages.id = "book-pages";
+    bookPages.id = `book-pages-${Math.floor(Math.random() * 100)}`;
     bookPages.innerText = `${book.pages} pages`;
     bookPages.style.fontSize = '2em';
     bookDiv.append(bookPages);
 
-    let bookIsread = document.createElement('div');
-    bookIsread.id = "book-Isread";
-    if (book.isRead == false) {
-        bookIsread.innerText = 'Not Read';
-    } else { bookIsread.innerText = 'Finished'; }
-
-    bookIsread.style.fontSize = '2em';
-    bookDiv.append(bookIsread);
+    let bookIsRead = document.createElement('div');
+    bookIsRead.id = `book-Isread-${Math.floor(Math.random() * 100)}`;
+    if (book.isRead === true) {
+        bookIsRead.style.color = 'green';
+        bookIsRead.innerText = 'Finished';
+        console.log(`The book ${book.title} isRead status is TRUE`);
+    } else {
+        bookIsRead.style.color = 'red';
+        bookIsRead.innerText = 'Not read yet';
+        console.log(`The book ${book.title} isRead status is FALSE`);
+    }
+    bookIsRead.style.fontSize = '2em';
+    bookDiv.append(bookIsRead);
 
     // Adds the button to change the read value
-    let changeReadStatus = document.createElement('button');
-    changeReadStatus.id = 'change-read';
-    changeReadStatus.classList.add('read-change-button')
-    bookDiv.append(changeReadStatus)
+    let changeReadButton = document.createElement('button');
+    changeReadButton.id = `change-read-button-${Math.floor(Math.random() * 100)}`;
+    changeReadButton.classList.add('read-change-button')
+    bookDiv.append(changeReadButton)
 
-    if (bookIsread.innerText = 'Not Read') {
-        changeReadStatus.innerText = 'Finished';
-    } else if (bookIsread.innerText = 'Finished') {
-        changeReadStatus.innerText = 'Not Read';
+    // Changes the text in the changeReadButton according to bookIsRead status.
+    if (bookIsRead.innerText === 'Not read yet') {
+        changeReadButton.innerText = 'Finish!';
+    } else if (bookIsRead.innerText === 'Finished') {
+        changeReadButton.innerText = 'Not Read Yet!';
     }
 
-    changeReadStatus.addEventListener('click', (e) => {
+    changeReadButton.addEventListener('click', (e) => {
         // Changes the read status in the html and changes the read value of the book object in the array
         if (book.isRead == false) {
-            e.target.parentElement.children[2].innerText = 'Finished';
+            e.target.parentElement.children[3].innerText = 'Finished';
             myLibrary[index].isRead = true;
-            changeReadStatus.innerText = 'Not Read';
+            changeReadButton.innerText = 'Not Read Yet!';
+            e.target.parentElement.children[3].style.color = 'green';
         } else {
-            e.target.parentElement.children[2].innerText = 'Not Read';
+            e.target.parentElement.children[3].innerText = 'Not Read Yet!';
             myLibrary[index].isRead = false;
-            changeReadStatus.innerText = 'Finished';
+            changeReadButton.innerText = 'Finish!';
+            e.target.parentElement.children[3].style.color = 'red';
         }
     });
 
